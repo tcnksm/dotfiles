e4ae6e1ef81c317ff19170361357767ff37c85fc
tcnksm <nsd22843@gmail.com>
2013-08-11 21:24:40 +0900

    Add oh-my-zsh settings

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,21 +3,30 @@
 # setup oh-my-zsh environment by creating simbolic links.
 # 
 
-# if [ ! -d ~/.oh-my-zsh ] ; then
-#     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-# fi
-# ln -s $(PWD)/theme/tc.zsh-theme $OH_MY_ZSH_HOME/themes/tc.zsh-theme
+
+readonly DOT_FILES=( zshrc.global zshrc.alias zshrc.osx zshrc.linux )
+
+mv ~/.zshrc ~/.zshrc.org
+ln -s $(PWD)/zshrc ~/.zshrc
 
 [ ! -d ~/.zsh ] && mkdir ~/.zsh
-[ ! -f ~/.zshrc ] && ln -s $(PWD)/zshrc ~/.zshrc
 
-for file in `ls`
+for file in ${DOT_FILES[@]}
 do
-    if [ ! -f ~/.zsh/$file -a ! "${file}" = "setup.sh" -a ! "${file}" = "zshrc" ]; then
-	echo "ln -s $(PWD)/$file ~/.zsh/.$file"
+    if [ ! -f ~/.zsh/$file ]; then
+        echo "ln -s $(PWD)/$file ~/.zsh/$file"
         ln -s $(PWD)/$file ~/.zsh/$file
     fi 
 done
+
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+if [ ! -f ~/.oh-my-zsh/themes/tc.zsh-theme ]; then
+    ln -s $(PWD)/oh-my-zsh-theme/tc.zsh-theme ~/.oh-my-zsh/themes/tc.zsh-theme
+fi
+
 
 
 
