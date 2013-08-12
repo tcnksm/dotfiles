890dae1824ef7a94db3f3e8f6d4eb5c8e95c7c60
tcnksm <nsd22843@gmail.com>
2013-08-12 15:19:25 +0900

    Modify for tmux clipbord setting

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -5,10 +5,10 @@
 # 
 
 if [ ! -d ~/.tmux-powerline ] ; then
-    git clone git://github.com/erikw/tmux-powerline.git ~/.tmux-powerline
+    git clone https://github.com/erikw/tmux-powerline.git ~/.tmux-powerline
 fi
 
-ln -s $(PWD)/tmux.conf ~/.tmux.conf
-ln -s $(PWD)/tmux-powerline-theme.sh ~/.tmux-powerline/themes/mytheme.sh
-ln -s $(PWD)/tmux-powerlinerc ~/.tmux-powerlinerc
+ln -s `pwd`/tmux.conf ~/.tmux.conf
+ln -s `pwd`/tmux-powerline-theme.sh ~/.tmux-powerline/themes/mytheme.sh
+ln -s `pwd`/tmux-powerlinerc ~/.tmux-powerlinerc
 
