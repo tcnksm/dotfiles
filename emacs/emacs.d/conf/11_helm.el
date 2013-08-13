50436a8702a924d1302d20b4d9d3c694fbbd71e0
tcnksm <nsd22843@gmail.com>
2013-08-13 18:04:52 +0900

    Add helm function

--- emacs/emacs.d/conf/11_helm.el
+++ emacs/emacs.d/conf/11_helm.el
@@ -5,3 +5,9 @@
 (require 'helm-config)
 (helm-mode 1)
 
+;; -------------------------------------------------
+;; Keybind
+;; -------------------------------------------------
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list) ;; C-x b is also
+
