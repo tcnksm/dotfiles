c004a103188a3638e7ac21ae7e8b1b8aaea11a95
tcnksm <nsd22843@gmail.com>
2013-08-26 09:29:44 +0900

    Add key

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -65,3 +65,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
