d88e5abadefcf87ecf34bb0932cb989623a8b334
tcnksm <nsd22843@gmail.com>
2013-10-03 08:54:44 +0900

    Add scss mode

---
+++ emacs.d/conf/51_scss.el
@@ -0,0 +1,6 @@
+;; ==================================================
+;; SASS SETTING
+;; ==================================================
+(require 'scss-mode nil t)
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+
