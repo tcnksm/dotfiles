42f7043f5d287a9c2057e6cb75c2c36dac660a7a
taichi.nakashima@mail.rakuten.com <taichi.nakashima@mail.rakuten.com>
2013-11-27 10:57:10 +0900

    Add some alias

--- emacs.d/conf/50_ruby.el
+++ emacs.d/conf/50_ruby.el
@@ -7,9 +7,11 @@
 ;; --------------------------------------------------
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.cap$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Berksfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
