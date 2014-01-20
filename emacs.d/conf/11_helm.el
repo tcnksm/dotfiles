191b86695bd7d4e1f6471b1ad114b5a3a07476ae
tcnksm <nsd22843@gmail.com>
2014-01-20 17:41:37 +0900

    Edit helm mode

--- emacs.d/conf/11_helm.el
+++ emacs.d/conf/11_helm.el
@@ -6,8 +6,30 @@
 (helm-mode 1)
 
 ;; -------------------------------------------------
-;; Keybind
+;; Keybind (run)
 ;; -------------------------------------------------
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "M-y")     'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list) ;; C-x b is also
+(global-set-key (kbd "C-x r")   'helm-recentf)
+(global-set-key (kbd "C-x C-z") 'helm-resume)
+(global-set-key (kbd "C-x C-g") 'helm-do-grep)
+
+;; -------------------------------------------------
+;; Keybind (while executing helm)
+;; -------------------------------------------------
+(define-key helm-read-file-map  (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map  (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map  (kbd "C-f") 'helm-execute-persistent-action)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+
+;; -------------------------------------------------
+;; Occur
+;; -------------------------------------------------
+(global-set-key (kbd "C-x C-o")          'helm-occur)
+(define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
+
+;; -------------------------------------------------
+;; Etc
+;; -------------------------------------------------
+(setq helm-ff-auto-update-initial-value nil)
 
