c13d14a8b8c0c6880352e79c8e762c53369365d3
tcnksm <nsd22843@gmail.com>
2014-02-07 16:05:46 +0900

    Add groovy mode

---
+++ emacs.d/conf/50_groovy.el
@@ -0,0 +1,18 @@
+;; ==================================================
+;; GROOVY MODE
+;; ==================================================
+
+;;; turn on syntax highlighting
+(global-font-lock-mode 1)
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
---
+++ emacs.d/elpa/groovy-mode-20131129.409/groovy-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "groovy-mode" "20131129.409" "Groovy mode derived mode" 'nil)
