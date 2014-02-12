6574a43554aa7d6c8ecd64dfe6534847e9348108
taichi.nakashima@mail.rakuten.com <taichi.nakashima@mail.rakuten.com>
2014-02-12 09:46:30 +0900

    Modify groovy settings

--- emacs.d/conf/50_groovy.el
+++ emacs.d/conf/50_groovy.el
@@ -2,17 +2,16 @@
 ;; GROOVY MODE
 ;; ==================================================
 
-;;; turn on syntax highlighting
-(global-font-lock-mode 1)
+;; ;;; turn on syntax highlighting
+;; (global-font-lock-mode 1)
 
 ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
-(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
-(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
-(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
-(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+(require 'groovy-mode nil t)
+(add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
 
-;;; make Groovy mode electric by default.
-(add-hook 'groovy-mode-hook
-          '(lambda ()
-             (require 'groovy-electric)
-             (groovy-electric-mode)))
+;; ;;; make Groovy mode electric by default.
+;; (add-hook 'groovy-mode-hook
+;;           '(lambda ()
+;;              (require 'groovy-electric)
+;;              (groovy-electric-mode)))
