bd5c96913667bc18a1b5bb6adc4d8d16a38b2db8
tcnksm <nsd22843@gmail.com>
2014-09-02 14:27:24 +0900

    Delete unnecessary files

--- emacs.d/conf/13_auto_highlight.el
+++ emacs.d/conf/13_auto_highlight.el
@@ -1,26 +1,7 @@
-;; ==================================================
-;; Auto highlight symbol
-;; ==================================================
-(require 'auto-highlight-symbol nil t)
-(global-auto-highlight-symbol-mode t)
-
 ;; --------------------------------------------------
-;; Highlight symblo
+;; Highlight symbol
 ;; --------------------------------------------------
 (require 'highlight-symbol nil t)
 
 (global-set-key (kbd "C-]") 'highlight-symbol-at-point)
 (global-set-key (kbd "M-]") 'highlight-symbol-remove-all)
-
-
-
-
-
-
-
-
-
-
-
-
-
