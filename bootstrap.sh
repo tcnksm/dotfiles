f44ed475aa41b81a9817fe5b4a0ef68d00ac10f6
tcnksm <nsd22843@gmail.com>
2014-11-06 20:05:11 +0900

    Make bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+set -e
+
+# Install homebrew packages
+./brewfile.sh
+
+# Install emacs cask
+pushd emacs.d
+cask install
+popd
+
+# Enable dotfile, make symbolic link to `$HOME` directory
+rake setup
+
+# Install tmux-plugin manager
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+
