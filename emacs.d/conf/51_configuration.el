c1b064a248617cc82328e5e6359eb49b60e5cdf7
tcnksm <nsd22843@gmail.com>
2015-06-16 10:01:20 +0900

    Add new plugin

--- emacs.d/conf/51_configuration.el
+++ emacs.d/conf/51_configuration.el
@@ -7,6 +7,7 @@
 ;; --------------------------------------------------
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("spec" . yaml-mode))
 (add-hook 'yaml-mode-hook
           '(lambda ()
              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
