603aff56d7fb772997a30eee4919f50f83d15bbc
tcnksm <nsd22843@gmail.com>
2016-02-23 11:46:20 +0900

    Stop using oh-my-zsh

--- zsh/install-tools.sh
+++ zsh/install-tools.sh
@@ -1,37 +1,7 @@
 #!/bin/bash
 
-info() {
-    echo -e "\033[34m$@\033[m" # blue
-}
-
-warn() {
-    echo -e "\033[33m$@\033[m" # yellow
-}
-
-error() {
-    echo -e "\033[31m$@\033[m" # red
-}
-
-unset https_proxy
-
-if [[ ! -d ~/.oh-my-zsh ]]; then
-    info "Install zsh-syntax-highlighting"
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [[ ! -d ~/.gitcontrib ]]; then
+    info "Install git contrib/completion scripts"
+    curl -L --create-dirs -o ~/.gitcontrib/git-completion.zsh "https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh"
+    curl -L --create-dirs -o ~/.gitcontrib/git-prompt.sh "https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh"
 fi 
-
-if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
-    info "Install zsh-syntax-highlighting"
-    pushd ~/.oh-my-zsh/custom/plugins
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-    popd
-fi 
-
-if [[ ! -f ~/.oh-my-zsh/themes/beer.zsh-theme ]]; then
-    info "Install oh-my-zsh-theme"
-    curl -fsSL https://raw.github.com/tcnksm/oh-my-zsh-beer-theme/master/beer.zsh-theme >> ~/.oh-my-zsh/themes/beer.zsh-theme
-fi 
-
-if [[ ! -d ~/.zjump ]]; then
-    info "Install z"
-    git clone https://github.com/rupa/z.git ~/.zjump
-fi
