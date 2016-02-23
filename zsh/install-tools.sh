c456dee40006fe73148eecdb705fd359d83e2b2f
tcnksm <nsd22843@gmail.com>
2016-02-23 18:47:42 +0900

    Remove

--- zsh/install-tools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ ! -d ~/.gitcontrib ]]; then
-    echo "Install git contrib/completion scripts"
-    curl -L --create-dirs -o ~/.gitcontrib/git-completion.zsh "https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh"
-    curl -L --create-dirs -o ~/.gitcontrib/git-prompt.sh "https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh"
-fi 
