c4aad71087a01f65a9dccf7a65c83071d108660e
tcnksm <nsd22843@gmail.com>
2016-02-23 19:43:23 +0900

    Remove old pkg directory

--- go/install.sh
+++ go/install.sh
@@ -10,6 +10,8 @@ if [ -d ${HOME}/.go/${GOVERSION} ]; then
     exit 1
 fi
 
+rm -fr ${GOHOME}/pkg
+
 mkdir -p ${HOME}/.go/${GOVERSION}
 curl https://storage.googleapis.com/golang/go${GOVERSION}.darwin-amd64.tar.gz \
     | tar xvzf - -C ${HOME}/.go/${GOVERSION}/ --strip-components=1
