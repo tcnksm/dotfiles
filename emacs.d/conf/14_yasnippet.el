4a3d5b35233d2f9c909461ddc25b2e1b9fa19190
tcnksm <nsd22843@gmail.com>
2016-02-29 11:04:02 +0900

    Added new config

--- emacs.d/conf/14_yasnippet.el
+++ emacs.d/conf/14_yasnippet.el
@@ -11,6 +11,7 @@
 ;; -------------------------------------------------
 ;; Keybind
 ;; -------------------------------------------------
+(define-key yas-minor-mode-map (kbd "C-x i u") 'yas-insert-snippet)
 (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
 (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
 (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
