e353c73a7cc275d5bf1326f18dfa3f99561fcfe5
tcnksm <nsd22843@gmail.com>
2016-05-15 11:01:16 +0900

    Add peco function

---
+++ peco/git-branch.zsh
@@ -0,0 +1,15 @@
+function peco-git-branch-checkout () {
+    local selected_branch_name="$(git branch -a | peco | tr -d ' ')"
+        case "$selected_branch_name" in
+                *-\>* )
+                    selected_branch_name="$(echo ${selected_branch_name} | perl -ne 's/^.*->(.*?)\/(.*)$/\2/;print')";;
+                remotes* )
+                    selected_branch_name="$(echo ${selected_branch_name} | perl -ne 's/^.*?remotes\/(.*?)\/(.*)$/\2/;print')";;
+        esac
+        if [ -n "$selected_branch_name" ]; then
+            BUFFER="git checkout ${selected_branch_name}"
+            zle accept-line
+        fi
+        zle clear-screen
+}
+zle -N peco-git-branch-checkout
