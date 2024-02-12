12e080087b7f734eaf7f6fc723d952f6576a9ec9
Taichi Nakashima <tcnksm@users.noreply.github.com>
2024-02-12 18:23:23 +0900

    Update install-go.sh

--- script/install-go.sh
+++ script/install-go.sh
@@ -4,20 +4,20 @@ set -e
 
 PKGS=(
     # General
-    github.com/motemen/ghq
+    github.com/motemen/ghq@latest
 
     # Go development
-    github.com/davidrjenni/reftools/cmd/fillstruct
-    github.com/mdempsky/gocode
-    golang.org/x/tools/gopls
-    golang.org/x/tools/cmd/goimports
-    golang.org/x/tools/cmd/gorename
-    golang.org/x/tools/cmd/benchcmp
-    golang.org/x/tools/cmd/present
-    golang.org/x/tools/cmd/guru
+    # github.com/davidrjenni/reftools/cmd/fillstruct
+    # github.com/mdempsky/gocode
+    # golang.org/x/tools/gopls
+    # golang.org/x/tools/cmd/goimports
+    # golang.org/x/tools/cmd/gorename
+    # golang.org/x/tools/cmd/benchcmp
+    # golang.org/x/tools/cmd/present
+    # golang.org/x/tools/cmd/guru
 )
 
 for pkg in ${PKGS[@]}
 do
-    go get -u -v $pkg
+    go install -u -v $pkg
 done
