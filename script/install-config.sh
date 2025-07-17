b0f7a80ff7a474c93c1549d4916f5401fc5a18d0
tcnksm <taichi@hey.com>
2025-07-17 12:35:17 +0900

    Add GNUPG config

--- script/install-config.sh
+++ script/install-config.sh
@@ -24,6 +24,13 @@ fi
 if [ ! -f ${HOME}/.claude/settings.json ]; then
   echo "[INFO] Place ${HOME}/.claude/settings.json"
   mkdir -p $HOME/.claude
-  ln -sf ${DIR}/settings.json ${HOME}/.claude/settings.json
+  ln -sf ${DIR}/claude/settings.json ${HOME}/.claude/settings.json
+fi
+
+if [ ! -f ${HOME}/.gnupg/gpg-agent.conf ]; then  
+  echo "[INFO] Place ${HOME}/.gnupg/gpg-agent.conf"
+  echo "[INFO] Run 'gpgconf --kill gpg-agent' to reload the config"
+  mkdir -p $HOME/.gnupg
+  ln -sf ${DIR}/gnupg/gpg-agent.conf ${HOME}/.gnupg/gpg-agent.conf
 fi
 
